8e709b31d1aaef8818a0042b555a0794f572b02e
nihadmm <nihadmm@code.edu.az>
2022-08-05 12:21:17 +0400

    HomeTask2

---
+++ HomeTask2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HomeTask2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 21; i++)
+                Console.WriteLine(i * 2);
+        }
+    }
+}
